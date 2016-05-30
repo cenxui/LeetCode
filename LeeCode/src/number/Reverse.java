de2eb135783f1c453523d2e32eb4ebd10c6f3151
cenxui lin <b97201017@ntu.edu.tw>
2016-05-30 13:50:35 +0800

    reverse integer

--- LeeCode/src/number/Reverse.java
+++ LeeCode/src/number/Reverse.java
@@ -3,11 +3,11 @@ package number;
 public class Reverse {
 
 	public static void main(String[] args) {
-		System.out.println(reverseBit(43261596));
-
+//		System.out.println(reverseBit(43261596));
+		System.out.println(reverseInteger(-123));
 	}
 	
-	private static int reverseBit(int n) {
+	public static int reverseBit(int n) {
 		int result = 0;
         for (int i = 0; i<32; i++) {
         	result <<= 1;
@@ -17,5 +17,27 @@ public class Reverse {
             n >>= 1;
         }
         return result;
+	}
+	
+	public static int reverseInteger(int n) {
+		if (n >= 0) {
+			return reverseUnsingInteger(n);
+		}else {
+			return reverseUnsingInteger(-1*n)*-1;
+		}
+	}
+	
+	private static int reverseUnsingInteger(int n) {
+		char[] temp = String.valueOf(n).toCharArray();
+		
+		char[] number = new char[temp.length];
+		int tempPosition = temp.length-1; 
+		for (int i = 0;i <= tempPosition;i++) {
+			number[i] = temp[tempPosition - i];
+		}
+		
+		return Integer.valueOf(String.valueOf(number)).intValue();
+		
+		
 	}
 }
