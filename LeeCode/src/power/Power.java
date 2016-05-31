dd4d9a8b380db3b2032fdf984c0e7d1d51cb026c
cenxui lin <b97201017@ntu.edu.tw>
2016-05-31 15:59:51 +0800

    is power of four

--- LeeCode/src/power/Power.java
+++ LeeCode/src/power/Power.java
@@ -4,20 +4,24 @@ public class Power {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int b = 30;
-		for (int i = 0; i<5; i++) {
-			b = b >>1;
-		System.out.println(b);
+		
+		System.out.println(isPowerFour(1));
+	}
+	
+	public static boolean isPowerTwo(int num) {
+		return num <= 0 ? false:(num & (num-1)) == 0;
+	}
+	
+	public static boolean isPowerFour(int num) {
+		if (isPowerTwo(num)) {
+			while (num>0) {			
+				if (num == 1)
+					return true;
+				num >>= 2; 
+			}
 		}
 		
-	}
-	
-	public static boolean isPowerTwo(int n) {
-		return n <= 0 ? false:(n & (n-1)) == 0;
-	}
-	
-	private static boolean isPowerFour(int n) {
-		return isPowerTwo(n) && ((n>>1)&n) == 0; 
+		return false; 
 	}
 	
 //	public static boolean isPowerOfThree(int n) {
