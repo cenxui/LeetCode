30a6c15d63a4d06af5a0eb7a400b5df1054856d6
cenxui lin <b97201017@ntu.edu.tw>
2016-06-02 17:03:39 +0800

    min stack

---
+++ LeeCode/src/set/MinStack.java
@@ -0,0 +1,56 @@
+package set;
+
+public class MinStack {
+	private Stack mStack;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public MinStack() {
+        
+    }
+    
+    public void push(int x) {
+        Stack stack = new Stack();
+        stack.value = x;
+        if (mStack == null) {
+			mStack = stack;
+		}else {
+			stack.stack = mStack;
+			mStack = stack;
+		}
+    }
+    
+    public void pop() {
+        if (mStack != null) {
+        	mStack = mStack.stack;
+        }
+    }
+    
+    public int top() {
+    	if (mStack != null) {
+    		return mStack.value;
+    	}
+        return 0;
+    }
+    
+    public int getMin() {
+    	int min = Integer.MAX_VALUE;
+    	Stack stack = mStack;
+    	while (stack != null) {
+			if (stack.value < min) {
+				min = stack.value;
+			}
+			stack = stack.stack;
+		}
+    	
+        return min;
+    }
+    
+    private class Stack {
+    	private int value;
+    	private Stack stack;
+    }
+}
