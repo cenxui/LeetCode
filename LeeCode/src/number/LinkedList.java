5a93f1244c13709bae69f8a691d2dd2255984e9d
cenxui lin <b97201017@ntu.edu.tw>
2016-06-05 15:28:30 +0800

    delete duplicates

--- LeeCode/src/number/LinkedList.java
+++ LeeCode/src/number/LinkedList.java
@@ -3,16 +3,17 @@ package number;
 public class LinkedList {
 
 	public static void main(String[] args) {
-		
+
 		ListNode listNode = getListNode(1, 6);
-//		printLisNode(listNode);
+		// printLisNode(listNode);
 		ListNode resultNode = oddEvenList(listNode);
 		printLisNode(resultNode);
 	}
-	
+
 	/**
 	 * 
-	 * @param head input linked list
+	 * @param head
+	 *            input linked list
 	 * @return the reverse linked list
 	 */
 
@@ -26,69 +27,110 @@ public class LinkedList {
 			head = head.next;
 			tempNode = resultNode;
 			resultNode = new ListNode(head.val);
-			resultNode.next = tempNode;				
+			resultNode.next = tempNode;
 		}
 		return resultNode;
 	}
 
 	public static void deleteNode(ListNode node) {
-		 node.val = node.next.val;
-		 node.next = node.next.next;	        
-	 }
-	
-	public static void addNode(ListNode node , ListNode addNode ) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+
+	public static void addNode(ListNode node, ListNode addNode) {
 		ListNode listNode = node.next;
 		node.next = addNode;
 		addNode.next = listNode;
 	}
-	 
+
 	public static ListNode oddEvenList(ListNode head) {
 		if (head == null) {
 			return null;
 		}
-						
+
 		ListNode oddListNode = head;
 		if (head.next == null) {
 			return oddListNode;
 		}
-		
+
 		if (head.next.next == null) {
 			return oddListNode;
 		}
 
 		ListNode evenNode;
 		evenNode = new ListNode(head.next.val);
-		
+
 		ListNode evenListNode = evenNode;
-					
+
 		head.next = head.next.next;
 		head = head.next;
-		
-		while (head.next != null) {		
+
+		while (head.next != null) {
 			evenNode.next = new ListNode(head.next.val);
 			evenNode = evenNode.next;
 			if (head.next.next == null) {
 				break;
 			}
 			head.next = head.next.next;
-			head = head.next;			
+			head = head.next;
 		}
 		head.next = evenListNode;
 		return oddListNode;
 	}
-	
-	public static void printLisNode(ListNode listNode) {
-		while (listNode != null) {
-			System.out.println(listNode.val);
-			listNode = listNode.next;
+
+	/**
+	 * deletDuplicates ListNode
+	 * @param head 
+	 * @return deletDuplicates ListNod
+	 */
+	public static ListNode deleteDuplicates(ListNode head) {		
+		if (head == null) {
+			return head;
+		}
+		
+		ListNode resultNode = head;
+		ListNode checkNode;
+		int headValue;
+
+		while (head!= null) {
+			headValue = head.val;
+			checkNode = head;
+			while (checkNode != null&& headValue == checkNode.val ) {
+				checkNode = checkNode.next;
+			}
+			head.next = checkNode;
+			head = head.next;
+		}
+
+		return resultNode;
+	}
+
+	/**
+	 * print the given list node to the last list node
+	 * 
+	 * @param listNode
+	 *            the head list node
+	 */
+
+	public static void printLisNode(ListNode head) {
+		while (head != null) {
+			System.out.println(head.val);
+			head = head.next;
 		}
 	}
-	
+
+	/**
+	 * 
+	 * @param from
+	 * @param to
+	 * @return
+	 */
+
 	public static ListNode getListNode(int from, int to) {
-		ListNode listNode = new ListNode(1);
+		ListNode listNode = new ListNode(from);
 		ListNode addNode;
 		ListNode endNode = listNode;
-		for (int i = from+1; i<to ;i++) {
+		for (int i = from + 1; i < to; i++) {
 			addNode = new ListNode(i);
 			endNode.next = addNode;
 			endNode = endNode.next;
