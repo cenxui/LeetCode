5beb3cf0c688a0973260cae529198c8c461aecc7
cenxui lin <b97201017@ntu.edu.tw>
2016-06-05 10:19:42 +0800

    odd even list

--- LeeCode/src/number/LinkedList.java
+++ LeeCode/src/number/LinkedList.java
@@ -4,7 +4,10 @@ public class LinkedList {
 
 	public static void main(String[] args) {
 		
-		
+		ListNode listNode = getListNode(1, 6);
+//		printLisNode(listNode);
+		ListNode resultNode = oddEvenList(listNode);
+		printLisNode(resultNode);
 	}
 	
 	/**
@@ -28,11 +31,71 @@ public class LinkedList {
 		return resultNode;
 	}
 
-	 public static void deleteNode(ListNode node) {
+	public static void deleteNode(ListNode node) {
 		 node.val = node.next.val;
 		 node.next = node.next.next;	        
 	 }
 	
+	public static void addNode(ListNode node , ListNode addNode ) {
+		ListNode listNode = node.next;
+		node.next = addNode;
+		addNode.next = listNode;
+	}
+	 
+	public static ListNode oddEvenList(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+						
+		ListNode oddListNode = head;
+		if (head.next == null) {
+			return oddListNode;
+		}
+		
+		if (head.next.next == null) {
+			return oddListNode;
+		}
+
+		ListNode evenNode;
+		evenNode = new ListNode(head.next.val);
+		
+		ListNode evenListNode = evenNode;
+					
+		head.next = head.next.next;
+		head = head.next;
+		
+		while (head.next != null) {		
+			evenNode.next = new ListNode(head.next.val);
+			evenNode = evenNode.next;
+			if (head.next.next == null) {
+				break;
+			}
+			head.next = head.next.next;
+			head = head.next;			
+		}
+		head.next = evenListNode;
+		return oddListNode;
+	}
+	
+	public static void printLisNode(ListNode listNode) {
+		while (listNode != null) {
+			System.out.println(listNode.val);
+			listNode = listNode.next;
+		}
+	}
+	
+	public static ListNode getListNode(int from, int to) {
+		ListNode listNode = new ListNode(1);
+		ListNode addNode;
+		ListNode endNode = listNode;
+		for (int i = from+1; i<to ;i++) {
+			addNode = new ListNode(i);
+			endNode.next = addNode;
+			endNode = endNode.next;
+		}
+		return listNode;
+	}
+
 	public static class ListNode {
 		int val;
 		ListNode next;
