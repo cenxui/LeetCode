ed21260f3e5ff63a2b5630ccabfcc6b84d27c913
cenxui lin <b97201017@ntu.edu.tw>
2016-06-06 15:14:18 +0800

    getCombination method && getRow method

--- LeeCode/src/set/Subsets.java
+++ LeeCode/src/set/Subsets.java
@@ -9,7 +9,40 @@ public class Subsets {
 	public static void main(String[] args) {
 //		int[] nums = {-13,11,11,0,-5,-14,12,-11,-11,-14,-3,0,-3,12,-1,-9,-5,-13,9,-7,-2,9,-1,4,-6,-13,-7,10,10,9,7,13,5,4,-2,7,5,-13,11,10,-12,-14,-5,-8,13,2,-2,-14,4,-8,-6,-13,9,8,6,10,2,6,5,-10,0,-11,-12,12,8,-7,-4,-9,-13,-7,8,12,-14,10,-10,14,-3,3,-15,-14,3,-14,10,-11,1,1,14,-11,14,4,-6,-1,0,-11,-12,-14,-11,0,14,-9,0,7,-12,1,-6};
 //		List<List<Integer>> list = threeSum(nums);
-		System.out.println(generate(5));
+		System.out.println(getRow(2));
+	}
+	
+	public static List<Integer> getRow(int rowIndex) {
+		List<Integer> result = new ArrayList<>();
+		for (int i = 0; i<= rowIndex; i++) {
+			result.add(getCombination(rowIndex, i));
+		}
+        return result;
+    }
+	
+	/**
+	 * useful combination method
+	 * @param n the number of elements
+	 * @param k the number you want to pick
+	 * @return the number of method
+	 */
+	
+	public static int getCombination(int n,int k) {
+		long result = 1;
+		
+		if (k == 0|| k == n) {
+			return (int)result;
+		}
+		
+		if (k>n/2) {
+			k = n-k;
+		}
+		
+		for (int i = 1; i<=k ;i++) {
+			result = result*n/i;
+			n = n-1;
+		}
+		return (int)result;
 	}
 	
 	/**
