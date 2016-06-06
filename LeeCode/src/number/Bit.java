2826632391c0f34ca66727c8eceba1c7fd615db0
cenxui lin <b97201017@ntu.edu.tw>
2016-06-06 23:45:39 +0800

    bit comments

--- LeeCode/src/number/Bit.java
+++ LeeCode/src/number/Bit.java
@@ -1,9 +1,12 @@
 package number;
 
+import java.util.Stack;
+
 public class Bit {
 	public static void main(String[] arg) {
-		System.out.println(numberOfBit(2147483647l));
-
+		String a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+		String b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+		System.out.println(a.length());
 	}
 	
 	public static int numberOfBit(long l) {
@@ -12,9 +15,40 @@ public class Bit {
 			if ((l&1) == 1) {			
 				sum = sum + 1;				
 			} 
-			System.out.println(l);
 			l = l>>1;
 		}
 		return sum;
+	}
+	
+	public static String addBinary(String a, String b) {
+        long result = stringToByte(a) + stringToByte(b);
+        return byteToString(result);
+    }
+	
+	public static long stringToByte(String a) {
+		char[] charA = a.toCharArray();
+		long result = 0;
+		for (int i = 0; i<charA.length;i++) {
+			result <<=1;
+			result = result + (long)charA[i]-48;
+		}
+		return result;
+	}
+	
+	public static String byteToString(long a) {
+		if (a == 0) {
+			return "0";
+		}
+		
+		Stack<Long> stack = new Stack<>();
+		while (a > 0) {
+			stack.push(a&1);
+			a >>= 1;
+		}
+		StringBuilder result = new StringBuilder();
+		while (stack.isEmpty() == false) {
+			result.append(stack.pop());
+		}
+		return result.toString();
 	}
 }
