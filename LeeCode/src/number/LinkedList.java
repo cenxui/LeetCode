6c4ada3dc65b7425ab77aa75c7d145e8efd52b9d
cenxui lin <b97201017@ntu.edu.tw>
2016-06-07 11:52:56 +0800

    remove element

--- LeeCode/src/number/LinkedList.java
+++ LeeCode/src/number/LinkedList.java
@@ -2,14 +2,85 @@ package number;
 
 import java.util.Stack;
 
+import org.junit.Ignore;
+
 public class LinkedList {
 
 	public static void main(String[] args) {
 
-		ListNode listNode = getListNode(1, 3);
-//		 printLisNode(listNode);
+		ListNode listNode = getListNode(1, 2);
+		printLisNode(listNode);
+//		listNode.next.val = 1;
+//		listNode.next.next.val = 1;
+//		listNode.next.next.next.next.val = 1;
+		System.out.println("refine");
+		printLisNode(listNode);
+		System.out.println("removeElements");
+		removeElements(listNode, 1);
+		 printLisNode(listNode);
 		
-		printLisNode(rotateRight(listNode,1));
+	}
+	
+	/**
+	 * remove all elements node equal val in head 
+	 * @param head the listNode we want to check
+	 * @param val the value we want to remove
+	 * @return the list node with no node value equal val
+	 */
+	
+	public static ListNode removeElements(ListNode head, int val) {
+		ListNode result = head;
+		if (result == null) {
+			return result;
+		}
+		while (head.next != null) {
+			if (head.next.val == val) {
+				head.next = head.next.next;
+			}else {
+				head = head.next;
+			}			
+		}
+		
+		if (result.val == val && result.next == null) {
+			result = null;
+			return result;
+		}
+		
+		if (result.val == val && result.next != null) {
+			result.val = result.next.val;
+			result.next = result.next.next;
+		}
+		
+		return result;        
+    }
+	
+	/**
+	 * this method with low efficiency and need to be improve to be accepted
+	 * @param head
+	 * @return
+	 */
+	
+	public static boolean hasCycle(ListNode head) {
+		ListNode check = head;
+		
+        while (head != null) {
+        	head = head.next;
+			if (head == check) {
+				return true;
+			}
+			head = head.next;
+		}
+        return false;
+    }
+	
+	/**
+	 * still need to program
+	 * @param listNode
+	 * @return
+	 */
+	@Ignore
+	public static ListNode getCycleListNode(ListNode listNode) {
+		return null;
 	}
 	
 	/**
