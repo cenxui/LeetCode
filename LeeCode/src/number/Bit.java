12e446d79dbfb54a5f7dac631be72e0a34659291
cenxui lin <b97201017@ntu.edu.tw>
2016-06-09 16:17:13 +0800

    add method countBits

--- LeeCode/src/number/Bit.java
+++ LeeCode/src/number/Bit.java
@@ -4,10 +4,19 @@ import java.util.Stack;
 
 public class Bit {
 	public static void main(String[] arg) {
-		String a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
-		String b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
-		System.out.println(a.length());
+		for (int i = 0; i <1000; i++) {
+			System.out.println(i+"nuber of bit" + numberOfBit(i) + "\n");
+		}
 	}
+	
+	public int[] countBits(int num) {
+        int[] result = new int[num+1];
+        for (int i = 0; i < result.length; i++) {
+			result[i] = numberOfBit(i);
+		}
+        
+        return result;
+    }
 	
 	public static int numberOfBit(long l) {
 		int sum = 0;
