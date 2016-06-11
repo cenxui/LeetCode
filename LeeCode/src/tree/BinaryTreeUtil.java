76c433a1bc893f3dac022ae191a568b1d606a021
cenxui lin <b97201017@ntu.edu.tw>
2016-06-12 00:05:45 +0800

    add method levelOrder

--- LeeCode/src/tree/BinaryTreeUtil.java
+++ LeeCode/src/tree/BinaryTreeUtil.java
@@ -1,8 +1,19 @@
 package tree;
 
+import java.util.List;
+
+import tree.BinaryTree.TreeNode;
+
 public class BinaryTreeUtil {
 	public static void main(String[] args) {
 		
 	}
 	
+	public static List<List<Integer>> treeNodeToList(TreeNode treeNode) {
+		return null;
+	}
+	
+	private static void addTreeValue(TreeNode treeNode) {
+		return;
+	}
 }
