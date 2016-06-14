325039115eb9ec5ad6edf56825d5fe1b8ea6edc2
cenxui lin <b97201017@ntu.edu.tw>
2016-06-14 14:05:28 +0800

    error method my power

--- LeeCode/src/power/Power.java
+++ LeeCode/src/power/Power.java
@@ -6,7 +6,13 @@ public class Power {
 		System.out.println(myPow(3, 5));
 		
 	}
-	
+	/**
+	 * wrong method
+	 * @param x
+	 * @param n
+	 * @return
+	 */
+	@Deprecated
 	public static double myPow(double x, int n) {
 		double result = 1;
 		for (int i = n; i>0; i>>= 1) {
