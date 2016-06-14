2b5eec84b4fa2f4f99af09d24ee6af55d8067773
cenxui lin <b97201017@ntu.edu.tw>
2016-06-14 16:15:50 +0800

    add isValid method

--- LeeCode/src/words/Word.java
+++ LeeCode/src/words/Word.java
@@ -1,92 +1,141 @@
 package words;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Word {
 	// private static String words = "the sky is blue";
 	private static String words = " 1";
 
 	public static void main(String[] arg) {
-		System.out.println(lengthOfLastWord(" shi "));
+		System.out.println(isValid("()"));
+		
 	}
-	
+
+	public static boolean isValid(String s) {
+		if (s.length()% 2 == 1) {
+			return false;
+		}
+		char[] chars = s.toCharArray();
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < chars.length; i++) {
+			if (stack.isEmpty()) {
+				if (chars[i] == ')' || chars[i] == ']' || chars[i] == '}') {
+					return false;
+				} else {
+					if (chars[i] == '(') {
+						stack.push(')');
+					} else if (chars[i] == '[') {
+						stack.push(']');
+					} else if (chars[i] == '{') {
+						stack.push('}');
+					}
+				}
+			} else {
+				if (chars[i] == ')' && chars[i] == stack.peek()) {
+					stack.pop();
+				} else if (chars[i] == ']' && chars[i] == stack.peek()) {
+					stack.pop();
+				} else if (chars[i] == '}' && chars[i] == stack.peek()) {
+					stack.pop();
+				} else if (chars[i] == '(') {
+					stack.push(')');
+				} else if (chars[i] == '[') {
+					stack.push(']');
+				} else if (chars[i] == '{') {
+					stack.push('}');
+				} else {
+					return false;
+				}
+			}
+		}
+
+		return stack.isEmpty();
+
+	}
+
 	/**
 	 * get the lest word length of a sentence
-	 * @param s the sentence
+	 * 
+	 * @param s
+	 *            the sentence
 	 * @return the last word of sentence
 	 */
-	
+
 	public static int lengthOfLastWord(String s) {
-        if (s == null || s.length() == 0) {
+		if (s == null || s.length() == 0) {
 			return 0;
 		}
-        
-        if (s.equals(" ")) {
+
+		if (s.equals(" ")) {
 			return 0;
 		}
-        
-        String[] result = s.split(" ");
-        if (result.length == 0) {
+
+		String[] result = s.split(" ");
+		if (result.length == 0) {
 			return 0;
 		}
-        
-        System.out.println("array length" + result.length);
-        
-        return result[result.length-1].length();
-    }
-	
+
+		System.out.println("array length" + result.length);
+
+		return result[result.length - 1].length();
+	}
+
 	/**
 	 * check strings if isomorphic or not
-	 * @param s string compared
-	 * @param t string compare
+	 * 
+	 * @param s
+	 *            string compared
+	 * @param t
+	 *            string compare
 	 * @return true if they have same isomorphic structure
 	 */
-	
+
 	public static boolean isIsomorphic(String s, String t) {
 		if (s == null && t == null) {
 			return true;
 		}
-		
-		if (s == null|| t == null) {
+
+		if (s == null || t == null) {
 			return false;
 		}
-		
+
 		if (s.length() != t.length()) {
 			return false;
 		}
-		
+
 		if (s.length() == 0 && s.length() == t.length()) {
 			return true;
 		}
-		
+
 		if (s.length() == 1 && s.length() == t.length()) {
 			return true;
 		}
-		
-        int[] isomorphicS = isomorphicString(s);
-        int[] isomorphicT = isomorphicString(t);
-        return Arrays.equals(isomorphicS, isomorphicT);
-    }
+
+		int[] isomorphicS = isomorphicString(s);
+		int[] isomorphicT = isomorphicString(t);
+		return Arrays.equals(isomorphicS, isomorphicT);
+	}
 
 	public static boolean wordPattern(String pattern, String str) {
 		if (pattern == null && str == null) {
-			return true;			
-		}
-		
-		if (pattern == null||pattern.length() == 0) {
-			return false;
-		}
-		
-		String[] strings = str.split(" ");	
-		
-		if (pattern.length() == 1&& strings.length == 1 ) {
 			return true;
 		}
-		
+
+		if (pattern == null || pattern.length() == 0) {
+			return false;
+		}
+
+		String[] strings = str.split(" ");
+
+		if (pattern.length() == 1 && strings.length == 1) {
+			return true;
+		}
+
 		if (pattern.length() != strings.length) {
 			return false;
 		}
-		
+
 		int[] isomorphicWord = isomorphicWords(strings);
 		int[] isomorphicString = isomorphicString(pattern);
 		return Arrays.equals(isomorphicString, isomorphicWord);
@@ -94,7 +143,9 @@ public class Word {
 
 	/**
 	 * get the isomorphic word type
-	 * @param word the word we want to isomorphic
+	 * 
+	 * @param word
+	 *            the word we want to isomorphic
 	 * @return the isomorphic structure
 	 */
 	public static int[] isomorphicWords(String[] word) {
@@ -126,10 +177,12 @@ public class Word {
 		}
 		return isomorphic;
 	}
-	
+
 	/**
 	 * get the isomorphic string type
-	 * @param string the string we want to isomorphic
+	 * 
+	 * @param string
+	 *            the string we want to isomorphic
 	 * @return the isomorphic type
 	 */
 
