6ccec325cfbcb8aa1c5156688d0732a98c35c58b
cenxui lin <b97201017@ntu.edu.tw>
2016-06-21 15:06:58 +0800

    redo singleNumber

--- LeeCode/src/number/Array.java
+++ LeeCode/src/number/Array.java
@@ -5,99 +5,109 @@ import java.util.Arrays;
 public class Array {
 
 	public static void main(String[] args) {
-		int[] array = { 1, 1,2, 3, 3 ,4, 5, 6 };
+		int[] array = { 1, 1, 2, 3, 3, 4, 5, 6 };
 
 		System.out.println(removeDuplicates(array));
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i]);
 		}
 	}
-	
-	public static int singleNumber(int[] nums) {
-        Arrays.sort(nums);
 
-        if (nums.length == 1 ||nums[0]!= nums[1] ) {
+	public static int singleNumber(int[] nums) {
+		Arrays.sort(nums);
+		if (nums.length == 1) {
 			return nums[0];
 		}
-        
-        for (int i = 1 ; i < nums.length-1; i++) {
-			if (nums[i] != nums[i-1] && nums[i]!= nums[i+1]){
+
+		for (int i = 0; i < nums.length - 1; i = i + 2) {
+			if (nums[i] != nums[i + 1]) {
 				return nums[i];
-			}			
-        }
-        
-        if (nums[nums.length-1]!= nums[nums.length-2]) {
-			return nums[nums.length-1];
+			}
 		}
-        return 0;
-    }
-	
-	public int[] plusOne(int[] digits) {		
+
+		if (nums[nums.length - 1] != nums[nums.length - 2]) {
+			return nums[nums.length - 1];
+		}
+
+		return 0;
+	}
+
+	public int singleNumberII(int[] nums) {
+
+		return 0;
+	}
+
+	public int[] plusOne(int[] digits) {
 		int length = digits.length;
-		int i = length -1;
-		while (i>=0 || digits[i] == 9) {
+		int i = length - 1;
+		while (i >= 0 || digits[i] == 9) {
 			digits[i] = 0;
 			i--;
 		}
-		
+
 		if (i == -1) {
-			int[] result = new int[length+1];
-			System.arraycopy(digits, 0, result, 1,length);
+			int[] result = new int[length + 1];
+			System.arraycopy(digits, 0, result, 1, length);
 			result[0] = 1;
 			return result;
 		}
-		digits[i] = digits[i] +1;
-		
-		
-        return digits;
-    }
+		digits[i] = digits[i] + 1;
 
-	
+		return digits;
+	}
+
 	/**
 	 * remove duplicates elements in array
-	 * @param nums the array we want to remove all duplicates
-	 * @return the elements number retain in array 
+	 * 
+	 * @param nums
+	 *            the array we want to remove all duplicates
+	 * @return the elements number retain in array
 	 */
-	
+
 	public static int removeDuplicates(int[] nums) {
 		if (nums.length == 0 || nums.length == 1) {
 			return nums.length;
 		}
-	
+
 		int check = Integer.MAX_VALUE;
 		int[] result = new int[nums.length];
 		int length = 0;
-        for (int i = 0; i < nums.length; i++) {
+		for (int i = 0; i < nums.length; i++) {
 			if (nums[i] != check) {
 				result[length] = nums[i];
 				check = nums[i];
 				length++;
 			}
 		}
-        System.arraycopy(result, 0, nums, 0, length);
-        return length;
-    }
-	
+		System.arraycopy(result, 0, nums, 0, length);
+		return length;
+	}
+
 	/**
-	 * given a none zero and has a majority element that means more the n/2 
-	 * in nums array
-	 * @param nums the element array
+	 * given a none zero and has a majority element that means more the n/2 in
+	 * nums array
+	 * 
+	 * @param nums
+	 *            the element array
 	 * @return the majority element in nums array
 	 */
-	
+
 	public static int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
-        
-    }
-	
+		Arrays.sort(nums);
+		return nums[nums.length / 2];
+
+	}
+
 	/**
-	 * remove all elements that equal val 
-	 * @param nums elements array
-	 * @param val the value we want to remove
+	 * remove all elements that equal val
+	 * 
+	 * @param nums
+	 *            elements array
+	 * @param val
+	 *            the value we want to remove
 	 * @return the elements number after we remove
 	 */
-	
+
 	public static int removeElement(int[] nums, int val) {
 		int length = nums.length;
 		for (int i = 0; i < nums.length; i++) {
@@ -106,15 +116,15 @@ public class Array {
 			}
 		}
 		int[] result = new int[length];
-		for (int i = 0 ,j = 0; j < length && i<nums.length; i++) {
+		for (int i = 0, j = 0; j < length && i < nums.length; i++) {
 			if (nums[i] != val) {
 				result[j] = nums[i];
 				j++;
 			}
 		}
 		System.arraycopy(result, 0, nums, 0, length);
-		return length;       
-    }
+		return length;
+	}
 
 	public static void rotate(int[] nums, int k) {
 		int length = nums.length;
@@ -129,7 +139,9 @@ public class Array {
 
 	/**
 	 * check array whether include duplicate element
-	 * @param nums the none zero length array we want to check
+	 * 
+	 * @param nums
+	 *            the none zero length array we want to check
 	 * @return true if it exists duplicate elements
 	 */
 	public static boolean containsDuplicate(int[] nums) {
@@ -148,11 +160,14 @@ public class Array {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * the method only can be used in prime length array
-	 * @param nums the array we want to rotate
-	 * @param k the rotate step
+	 * 
+	 * @param nums
+	 *            the array we want to rotate
+	 * @param k
+	 *            the rotate step
 	 */
 
 	public static void rotatePrimeLength(int[] nums, int k) {
