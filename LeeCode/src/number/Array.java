afc42f8c1004741253f93d8c5eb330d77080f282
cenxui lin <b97201017@ntu.edu.tw>
2016-06-21 14:47:13 +0800

    add singleNumber

--- LeeCode/src/number/Array.java
+++ LeeCode/src/number/Array.java
@@ -13,6 +13,25 @@ public class Array {
 		}
 	}
 	
+	public static int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+
+        if (nums.length == 1 ||nums[0]!= nums[1] ) {
+			return nums[0];
+		}
+        
+        for (int i = 1 ; i < nums.length-1; i++) {
+			if (nums[i] != nums[i-1] && nums[i]!= nums[i+1]){
+				return nums[i];
+			}			
+        }
+        
+        if (nums[nums.length-1]!= nums[nums.length-2]) {
+			return nums[nums.length-1];
+		}
+        return 0;
+    }
+	
 	public int[] plusOne(int[] digits) {		
 		int length = digits.length;
 		int i = length -1;
